393e318d5f6059fa25a35d472da767b544aab0ce
yjleev <sjlee081010@gmail.com>
2024-12-02 15:04:15 +0900

    test

---
+++ lecture/lecture.js
@@ -0,0 +1,6 @@
+const typeA = window.localStorage.getItem('A');
+const typeB = window.localStorage.getItem('B');
+const typeC = window.localStorage.getItem('C');
+const typeD = window.localStorage.getItem('D');
+
+console.log(typeA,typeB,typeC,typeD);
--- result/result.js
+++ result/result.js
@@ -3,4 +3,4 @@ const typeB = window.localStorage.getItem('B');
 const typeC = window.localStorage.getItem('C');
 const typeD = window.localStorage.getItem('D');
 
-console.log(typeA,typeB,typeC,typeD)
+console.log(typeA,typeB,typeC,typeD);
